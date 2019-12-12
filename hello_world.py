d3c95ce31b1ac67b94e74abfc03b7820e9dc1498
jules <avram.iulia.augusta@gmail.com>
2019-12-12 11:13:25 +0100

    Add hello world

---
+++ hello_world.py
@@ -0,0 +1,22 @@
+import pyglet
+
+window = pyglet.window.Window()
+
+label = pyglet.text.Label(
+    'Hello World',
+    font_name='Times New Roman',
+    font_size=36,
+    x=window.width//2, y=window.height - 80,
+    anchor_x='center', anchor_y='center'
+)
+
+image = pyglet.resource.image("static/img/spies.jpg")
+
+
+@window.event
+def on_draw():
+    window.clear()
+    image.blit(150, 20)
+    label.draw()
+
+pyglet.app.run()
